8d655e1eadeed260cf6f6c8274ed28c9c05209e7
olivierjdumoulin-hue <olivier.j.dumoulin@gmail.com>
2026-06-23 08:09:18 +0200

    Update SitRepWidgetProvider.java

--- app/src/main/java/fr/olivierdumoulin/sitrepdum/SitRepWidgetProvider.java
+++ app/src/main/java/fr/olivierdumoulin/sitrepdum/SitRepWidgetProvider.java
@@ -3,7 +3,6 @@ package fr.olivierdumoulin.sitrepdum;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
@@ -11,18 +10,22 @@ import android.widget.RemoteViews;
 public class SitRepWidgetProvider extends AppWidgetProvider {
 
     @Override
-    public void onUpdate(Context context,
-                         AppWidgetManager appWidgetManager,
-                         int[] appWidgetIds) {
+    public void onUpdate(
+            Context context,
+            AppWidgetManager appWidgetManager,
+            int[] appWidgetIds) {
 
         for (int appWidgetId : appWidgetIds) {
 
-            RemoteViews views =
-                    new RemoteViews(context.getPackageName(),
-                            R.layout.sitrep_widget);
+            RemoteViews views = new RemoteViews(
+                    context.getPackageName(),
+                    R.layout.sitrep_widget
+            );
 
-            Intent intent =
-                    new Intent(context, MainActivity.class);
+            Intent intent = new Intent(
+                    context,
+                    MainActivity.class
+            );
 
             PendingIntent pendingIntent =
                     PendingIntent.getActivity(
@@ -34,12 +37,7 @@ public class SitRepWidgetProvider extends AppWidgetProvider {
                     );
 
             views.setOnClickPendingIntent(
-                    R.id.widget_title,
-                    pendingIntent
-            );
-
-            views.setOnClickPendingIntent(
-                    R.id.widget_text,
+                    R.id.widget_root,
                     pendingIntent
             );
 
